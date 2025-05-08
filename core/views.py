040c4e0cc403d1b2bf6ed5ebfea560a2454992c4
Juan David Cruz Angel <161855153+Firewallrbn@users.noreply.github.com>
2025-05-07 23:30:59 -0500

    Fix syntax & split authors view and urls

--- core/views.py
+++ core/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 class AuthorsView(APIView):
     def get(self, request):
         return Response({
             "integrantes": [
                 {"nombre": "Juan David Cruz Angel", "código": "329805"},
-                {"nombre": "Julian  Zambrano", "código": " no lo se "}
+                {"nombre": "Julian Zambrano", "código": "???"},
             ]
         })
+
